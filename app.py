046bc333f92733bddfa22a1f5e8fa13327f7f95f
zala275 <ghanshyamzala171@gmail.com>
2026-09-03 15:51:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -138,6 +138,6 @@ def submit():
     })
 
 
- if __name__ == "__main__":
-      port = int(os.environ.get("PORT", 5000)) 
-      app.run(host="0.0.0.0", port=port) 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
